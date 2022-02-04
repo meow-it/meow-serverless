7252114be886af038a6a11eae93520cba76e5560
tharunoptimus <tharunoptimus@outlook.com>
2022-02-04 14:56:29 +0530

    Styled the index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,63 @@
-import '../css/style.css'
-import '../css/form.css'
 import Head from 'next/head'
-import Link from 'next/link'
+import Image from 'next/image'
+import kitty from "../images/kitty.png"
 
-function MyApp({ Component, pageProps }) {
+function MyApp() {
   return (
     <>
       <Head>
-        <title>Pet Care App</title>
+        <title>Meow It Serverless</title>
       </Head>
 
-      <div className="top-bar">
-        <div className="nav">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/new">
-            <a>Add Pet</a>
-          </Link>
-        </div>
+      <div className='wrapper'>
+        <Image
+          src={kitty}
+          width={200}
+          height={200}
+          alt='Meow It'
+        />
+        <h1>Meow It ServerLess</h1>
+        <h2>All APIs reside at <code>/api</code> </h2>
+      </div>
 
-        <img
-          id="title"
-          src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
-          alt="pet care logo"
-        ></img>
-      </div>
-      <div className="grid wrapper">
-        <Component {...pageProps} />
-      </div>
+      <style jsx global>{`
+        * {
+          font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+          font-weight: 400;
+        }
+
+        html, body {
+          min-height: 100vh;
+          margin: 0;
+          padding: 0;
+        }
+
+        code {
+          padding: 0.125em 0.25em;
+          background: #faf9fa;
+          border: 1px solid #d2d3d7;
+          font-size: .9em;
+          hyphens: none;
+          tab-size: 2;
+          text-align: left;
+          word-spacing: normal;
+          word-break: normal;
+          word-wrap: normal;
+          box-decoration-break: clone;
+          -webkit-box-decoration-break: clone;
+          font-family: SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace;
+        }
+
+        .wrapper {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+
+      `}</style>
     </>
   )
 }
