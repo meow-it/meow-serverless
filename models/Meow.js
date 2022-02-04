4cac60436f2c0974c1aaedc85f9d8b4151dd5a86
tharunoptimus <tharunoptimus@outlook.com>
2022-02-04 14:10:48 +0530

    Added meow schema

---
+++ models/Meow.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+let secondsToLive = 24 * 60 * 60
+const Schema = mongoose.Schema;
+const MeowSchema = new Schema({
+    text: { type: String, required: true, trim: true },
+    likes: { type: Number, default: 0 },
+    toxic: { type: Boolean, default: false },
+    location: { type: { type: String, default: 'Point' }, coordinates: [Number] },
+    name: { type: String, default: "" },
+    profilePic: { type: String, default: "" },
+    likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    isReviewed: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now, index: { expireAfterSeconds: secondsToLive } },
+});
+
+MeowSchema.index({ location: '2dsphere' });
+
+export default mongoose.models.Meow || mongoose.model("Meow", MeowSchema)
