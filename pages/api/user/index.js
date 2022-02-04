b4bb10970be5d5b04084fde615f5a59497ff27d5
tharunoptimus <tharunoptimus@outlook.com>
2022-02-04 14:47:20 +0530

    User API working fine

---
+++ pages/api/user/index.js
@@ -0,0 +1,32 @@
+import dbConnect from "../../../lib/dbConnect"
+import User from "../../../models/User"
+import animals from "../animals"
+
+export default async function handler(req, res) {
+	const { method } = req
+
+	await dbConnect()
+
+	switch (method) {
+		case "GET":
+			res.status(200).send({ message: "User API is online" })
+			break
+		case "POST":
+			try {
+        
+				let name = `${animals[Math.floor(Math.random() * animals.length)]} Chan`
+				let profilePic = `https://avatars.dicebear.com/api/bottts/${name.replace(/\s/g, "")}.svg`
+		
+				let user = await User.create({ name, profilePic })
+				res.status(201).send(user)
+		
+			} catch (err) {
+				res.status(200).send({ status: false, message: "Something happened with the Server 🤦‍♀️" })
+				console.log(err)
+			}
+			break
+		default:
+			res.status(400).json({ success: false })
+			break
+	}
+}
--- public/meows/index.js
+++ public/meows/index.js
@@ -1,5 +1,5 @@
 import dbConnect from '../../../lib/dbConnect'
-import Pet from '../../../models/Pet'
+import Meow from '../../../models/Meow'
 
 export default async function handler(req, res) {
   const { method } = req
